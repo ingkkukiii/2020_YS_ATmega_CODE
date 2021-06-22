9d2a51201b22b15a6ebb570f3e00f86fdff17990
ingkkukiii <80079407+ingkkukiii@users.noreply.github.com>
2021-06-22 12:16:03 +0900

    Update 01_led.c
    반복문을 사용하여 led 8개가 동시에 켜졌다 1초후 꺼지는 동작이 반복되는 프로그램 LV.1

--- 01_led.c
+++ 01_led.c
@@ -1,12 +1,15 @@
-include "01_led.h"
+/*
+	title : LED 동작 제어
+	subject : 반복문을 사용하여 led 8개가 동시에 켜졌다 1초후 꺼지는 동작이 반복되는 프로그램 LV.1
+*/
+#include "02_led_demo.h"
 
-//led 8개가 동시에 켜졌다 1초후 꺼지는 프로그램 LV.1 
 int main(void)  
 {
 	DDR_LED = 0xFF;
 	PORT_LED = 0x00;
 
-	for(;;)
+	while(1)
 	{
 		PORT_LED = 0xFF;
 		_delay_ms(1000);
