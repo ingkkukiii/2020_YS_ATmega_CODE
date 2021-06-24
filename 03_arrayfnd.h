cf9b7a2723f02e6bf469b0014862af279f7df4f2
ingkkukiii <80079407+ingkkukiii@users.noreply.github.com>
2021-06-25 00:51:10 +0900

    Update 03_arrayfnd.h

--- 03_arrayfnd.h
+++ 03_arrayfnd.h
@@ -1,12 +1,12 @@
 #include <avr/io.h>
 #include <avr/delay.h>
 
-#define 	DDR_FND		DDRA //DATA¼± 8bit
+#define 	DDR_FND		DDRA //DATAÂ¼Â± 8bit
 #define		PORT_FND	PORTA
 
-#define 	DDR_COM		DDRC //¼±ÅÃ¼± 4bit
+#define 	DDR_COM		DDRC //Â¼Â±Ã…ÃƒÂ¼Â± 4bit
 #define		PORT_COM	PORTC
 
 unsigned char FND_DATA_TBL[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 
                                 0x6D, 0x7D, 0x27, 0x7F, 0x6F, 
-								0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+				0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
